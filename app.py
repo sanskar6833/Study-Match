b5890b1ddd4375ebfdd9cba4fda3ba4213d8c823
Sanskar Sharma <sanskar.sharma.cse.2022@miet.ac.in>
2025-09-20 21:59:19 +0530

    Move registration button to center column

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ if st.session_state.page == "home":
 if st.session_state.page == "home":
     left,centre,right=st.columns(3)
 
-    with right:
+    with centre:
         st.button("🚀Register Here",type="primary", on_click=go_to_registration)
         
 # -----------------------------
